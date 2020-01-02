ef6aaf09cb6fb139ca2f13b7cc94952c0c721a8d
40823210 <40823210@gm.nfu.edu.tw>
2020-01-02 15:19:20 +0800

    w17考試_2

--- downloads/dart_ex/weight-html/weight.dart
+++ downloads/dart_ex/weight-html/weight.dart
@@ -1,9 +1,9 @@
 import "dart:html";
 GtoB(num g){
-return g*2.2;
+return g*2.20462262;
 }
 BtoG(num b){
-return b*0.45;
+return b*0.45359237;
 }
 main() {
     InputElement tempInput = querySelector("#temp");
@@ -19,9 +19,9 @@ convert(String data){
   number = data.substring(0, len-2);
   number = int.parse(number);
   if (type == "K" || type == "k"){
-     output.innerHtml = "公斤 $number kg = 英磅 ${GtoB(number).toStringAsFixed(2)} lb";
+     output.innerHtml = "公斤 $number kg = 英磅 ${GtoB(number).toStringAsFixed(4)} lb";
   } else if ((type == "L" || type == "l")){
-     output.innerHtml = "英磅 $number lb = 公斤 ${BtoG(number).toStringAsFixed(2)} kg";
+     output.innerHtml = "英磅 $number lb = 公斤 ${BtoG(number).toStringAsFixed(4)} kg";
   } else {
      output.innerHtml = "請輸入數字加上 KG 或 LB!";
   }
